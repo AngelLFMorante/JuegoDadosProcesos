303381a3a4c2fa46cf4b5d8d6356503797017d7a
cesar <cesvilar.s@gmail.com>
2021-02-10 21:08:04 +0100

    Cliente recibo datos de servidor;

---
+++ src/main/java/riotgamewizard/server/Partida.java
@@ -0,0 +1,53 @@
+package riotgamewizard.server;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Partida implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private Jugador anfitrion;
+	private List<Jugador> jugadores;
+
+	public Partida(String id, Jugador anfitrion) {
+		this.anfitrion  = anfitrion;
+		this.id = id;
+		this.jugadores = new ArrayList<>();
+		this.jugadores.add(anfitrion);
+	}
+	
+	//jugador invitado
+	public void annadirJugador(Jugador jugador) {
+		this.jugadores.add(jugador);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Jugador getAnfitrion() {
+		return anfitrion;
+	}
+
+	public void setAnfitrion(Jugador anfitrion) {
+		this.anfitrion = anfitrion;
+	}
+
+	public List<Jugador> getJugadores() {
+		return jugadores;
+	}
+
+	public void setJugadores(List<Jugador> jugadores) {
+		this.jugadores = jugadores;
+	}
+	
+	
+	
+}
